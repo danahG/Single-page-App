9af1dd9a3c0a89e3de5a5492e1b91a9f868e4d85
danahG <danielageorgieva3107@gmail.com>
2017-04-03 14:22:42 +0300

    responsive welcome anonymous view

--- scripts/app.js
+++ scripts/app.js
@@ -4,48 +4,6 @@ function startApp() {
     showHideNavigationLinks();
     showCorrectHomeView();
 
-    function showHideNavigationLinks() {
-        if(sessionStorage.getItem('authtoken') == null) {
-            // no logged in user
-            $('#navigation .anonymous').show();
-            $('#navigation .user-only').hide();
-            $('#spanLoggedInUser').text('');
-        } else {
-            // we have logged in user
-            $('#navigation .anonymous').hide();
-            $('#navigation .user-only').show();
-            $('#spanLoggedInUser').text('Welcome, ' + sessionStorage.getItem('username') + '!');
-        }
-    }
-
-    function showCorrectHomeView() {
-        if (sessionStorage.getItem('username')) {
-            showUserHomeView();
-        } else {
-            showAppHomeView();
-        }
-    }
-
-    function showUserHomeView() {
-        $('#viewUserHome h1').text('Welcome, ' + sessionStorage.getItem('username') + '!');
-        showView('viewUserHome');
-    }
-
-    function showAppHomeView() {
-        showView('viewAppHome');
-    }
-
-    function showView(viewName) {
-        // hide all views and show the selected view only
-        $('#container > section').hide();
-        $('#' + viewName).show();
-    }
-
-    function showLoginView() {
-        showView('viewLogin');
-        $('#loginForm').trigger('reset');
-    }
-
     // bind the navigation links anonymous
     $('#homeAppNavigationLink').click(showAppHomeView);
     $('#loginNavigationLink').click(showLoginView);
@@ -55,73 +13,28 @@ function startApp() {
 
 
     // TODO bind the form submit buttons
-    $("#formLogin").submit(loginUser);
-    $("#registerForm").submit(registerUser);
-    $("form").submit(function(event) { event.preventDefault() });
-
-    // Attach a global AJAX handler
-    // $(document).ajaxError(handleAjaxError);
-
-    const kinveyBaseUrl = 'https://baas.kinvey.com/';
-    const kinveyAppKey = 'kid_rkTWoZY2e';
-    const kinveyAppSecret = '8c7b22a18e02478ab63c215b7787200b';
-    const kinveyAppAuthHeaders = {
-        'Authorization': 'Basic' + btoa(kinveyAppKey + ':' + kinveyAppSecret)
-    };
-
-    function getKinveyUserAuthHeaders() {
-        return {
-            'Authorization': "Kinvey " + sessionStorage.getItem('authtoken'),
-        };
-    }
-
-    function loginUser() {
-        let userData = {
-            username: $('#loginForm input[name=username]').val(),
-            password: $('#loginForm inpu[name=password]').val()
-        };
-
-        $.ajax({
-            method: 'POST',
-            url: kinveyBaseUrl + 'user/' + kinveyAppKey + '/login',
-            headers: kinveyAppAuthHeaders,
-            data: userData,
-            success: loginSuccess
-        });
-
-        function loginSuccessful(userInfo) {
-            saveAuthInSession(userInfo);
-            showHideMenuLinks();
-            showUserHomeView();
-            showInfo('Login successful.');
-        }
-    }
+    $("#loginForm").submit(login);
+    $("#registerForm").submit(register);
+    $("form").submit(function(event) {
+        event.preventDefault();
+    });
 
 
-    function showRegisterView() {
-        $('#registerForm').trigger('reset');
-        showView('viewRegister');
-    }
 
-    function registerUser() {
-        let userData = {
-            username: $('#registerForm input[name=username]').val(),
-            password: $('#registerForm input[name=passwd]').val(),
-            // name: $('#formRegister input[name=name]').val()
-        };
-        $.ajax({
-            method: "POST",
-            url: kinveyBaseUrl + "user/" + kinveyAppKey + "/",
-            headers: kinveyAppAuthHeaders,
-            data: userData,
-            success: registerSuccessful
-        });
 
-        function registerSuccessful(userInfo) {
-            saveAuthInSession(userInfo);
-            showHideMenuLinks();
-            showUserHomeView();
-            showInfo('User registration successful.');
-        }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- scripts/authorization.js
+++ scripts/authorization.js
@@ -1,3 +1,67 @@
-/**
- * Created by Dana on 3/31/2017.
- */
+const kinveyBaseUrl = 'https://baas.kinvey.com/';
+const kinveyAppKey = 'kid_rkTWoZY2e';
+const kinveyAppSecret = '8c7b22a18e02478ab63c215b7787200b';
+const kinveyAppAuthHeaders = 'Basic ' + btoa(kinveyAppKey + ':' + kinveyAppSecret);
+
+// function getKinveyUserAuthHeaders() {
+//     return {
+//         'Authorization': 'Basic ' + sessionStorage.getItem('authtoken')
+//     }
+// }
+
+function saveAuthInSession(userInfo) {
+    let userAuth = userInfo._kmd.authtoken;
+    sessionStorage.setItem('authToken', userAuth);
+    let userId = userInfo._id;
+    sessionStorage.setItem('userId', userId);
+    let username = userInfo.username;
+    $('#spanMenuLoggedInUser').text('Welcome, ' + username + '!');
+    $('#viewUserHomeHeading').text('Welcome, ' + username + '!');
+}
+
+function register() {
+    let userData = {
+        username: $('#registerForm input[name=username]').val(),
+        password: $('#registerForm input[name=password]').val(),
+        name: $('#registerForm input[name=name]').val()
+    };
+    $.ajax({
+        method: "POST",
+        url: kinveyBaseUrl + "user/" + kinveyAppKey + "/",
+        headers: kinveyAppAuthHeaders,
+        data: userData,
+        success: registerSuccessful
+    });
+
+    function registerSuccessful(userInfo) {
+        saveAuthInSession(userInfo);
+        showHideMenuLinks();
+        showUserHomeView();
+        showInfo('User registration successful.');
+    }
+}
+
+function login() {
+    let userData = {
+        username: $('#loginForm input[name=username]').val(),
+        password: $('#loginForm input[name=password]').val()
+    };
+    $.ajax({
+        method: 'POST',
+        url: kinveyBaseUrl + 'user/' + kinveyAppKey + '/login',
+        headers: kinveyAppAuthHeaders,
+        data: userData,
+        success: loginSuccessful
+    });
+
+    function loginSuccessful(userInfo) {
+        saveAuthInSession(userInfo);
+        showHideNavigationLinks();
+        showUserHomeView();
+        showInfo('Login successful');
+    }
+}
+
+function logout() {
+
+}
